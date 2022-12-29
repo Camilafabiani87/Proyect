160f844e8558755e46e594515d66b63a6f3c9829
Camilafabiani87 <54865542+Camilafabiani87@users.noreply.github.com>
2022-12-29 14:09:51 -0300

    repository and services

---
+++ src/main/java/com/ecommerce/proyect/repositories/ClientRepository.java
@@ -0,0 +1,8 @@
+package com.ecommerce.proyect.repositories;
+
+import com.ecommerce.proyect.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long>{
+
+}
